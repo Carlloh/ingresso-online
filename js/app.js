83f59bdaab3883b6fae3d332e6d9396b27beb20c
Carlo Cardoso <cccimi@sapo.pt>
2023-12-26 17:48:35 +0000

    Realizar compra Pista

--- js/app.js
+++ js/app.js
@@ -1,6 +1,18 @@
 function comprar() {
     let tipo = document.getElementById('tipo-ingresso');
-    let qtd = document.getElementById('qtd');
-    alert(tipo.value);
-    alert(qtd.value);
+    let qtd = parseInt(document.getElementById('qtd').value);
+    if(tipo.value == 'pista') {
+        comprarPista(qtd);
+    }
+}
+
+function comprarPista(qtd) {
+    let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
+    if(qtd > qtdPista) {
+        alert('Quantidade indisponível para a Pista');
+    } else {
+        qtdPista = qtdPista - qtd;
+        document.getElementById('qtd-pista').textContent = qtdPista;
+        alert('Compra realizada com sucesso!');
+    }
 }
